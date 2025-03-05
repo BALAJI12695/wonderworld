23b367ef4edb88957a663d34b1fdf2485bd963f4
Balaji <balaji12695@gmail.com>
2025-03-05 12:59:12 +0530

    store mistake

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import App from './App';
-import store from './store'
+import {store} from './store'
 import { Provider } from 'react-redux';
 
 
